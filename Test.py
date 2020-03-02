828b58db2110a6b25636661adc8b5f4682c5f8d3
Nate <natewick005@gmail.com>
2020-03-01 16:09:35 -0800

    Added documentation and updated toString()

--- Test.py
+++ Test.py
@@ -4,11 +4,11 @@ first = Symptom("Cough")
 first.addCat("Lung")
 first.addSeverity(8)
 
-first.toString()
+print(first.toString())
 
 secondSymptom = Symptom("Pharyngitis")
 secondSymptom.addCat("Throat")
 secondSymptom.addSeverity(4)
 
-secondSymptom.toString()
+print(secondSymptom.toString())
 
