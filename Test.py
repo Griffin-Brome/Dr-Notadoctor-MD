bf36a7716d53347a9bad066f9212a0f1fe37df31
Nate <natewick005@gmail.com>
2020-03-01 16:51:28 -0800

    Added documentation to testing class

--- Test.py
+++ Test.py
@@ -1,31 +1,36 @@
 from Symptom import *
 from patient import *
 
+#Creating first Symptom
 first = Symptom("Cough")
 first.addCat("Lung")
 first.addSeverity(8)
 
-#print(first.toString())
-
+# Creating Second Symptom
 secondSymptom = Symptom("Pharyngitis")
 secondSymptom.addCat("Throat")
 secondSymptom.addSeverity(4)
 
-#print(secondSymptom.toString())
-
-
+# Creating first patient
 patient1 = patient("Bob", 29, "Male")
+# Adding symptom objects to Patient
 patient1.addSymptom(first)
 patient1.addSymptom(secondSymptom)
-
+# Get patient info(Auto prints)
 patient1.patientInfo()
 
+# Creating Second Patient
 patient2 = patient("Joe", 29, "Male")
+
+# Adding symptom object to patient
 patient2.addSymptom(first)
+# Creating third Symptom
 third = Symptom("Fatigue")
 third.addCat("neurological")
 third.addSeverity(10)
+# Adding symptom to patient
 patient2.addSymptom(third)
 
+# Get patient info(Auto prints)
 patient2.patientInfo()
 
