71c629586512380820cf4739960af8c258d2e870
Nate <natewick005@gmail.com>
2020-03-01 16:45:51 -0800

    Created Patient.py, do as you will

--- Test.py
+++ Test.py
@@ -1,14 +1,31 @@
 from Symptom import *
+from patient import *
 
 first = Symptom("Cough")
 first.addCat("Lung")
 first.addSeverity(8)
 
-print(first.toString())
+#print(first.toString())
 
 secondSymptom = Symptom("Pharyngitis")
 secondSymptom.addCat("Throat")
 secondSymptom.addSeverity(4)
 
-print(secondSymptom.toString())
+#print(secondSymptom.toString())
+
+
+patient1 = patient("Bob", 29, "Male")
+patient1.addSymptom(first)
+patient1.addSymptom(secondSymptom)
+
+patient1.patientInfo()
+
+patient2 = patient("Joe", 29, "Male")
+patient2.addSymptom(first)
+third = Symptom("Fatigue")
+third.addCat("neurological")
+third.addSeverity(10)
+patient2.addSymptom(third)
+
+patient2.patientInfo()
 
