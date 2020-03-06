efbe50c827a205c8945d40824909f2aaee30ea7a
wubbadukky <adcollins.d@gmail.com>
2020-03-06 10:15:21 -0800

    Update doctor.py

--- doctor.py
+++ doctor.py
@@ -1,24 +1,29 @@
-# This is the main method, most important methods will be run from here
-if __name__ == "main":
-    main()
-
-def main():
-    greet_user()
-    get_basic_info()
-
-def greet_user():
-
-def get_basic_info():
-    
-'''
-Asks user several questions about how they are feeling, then determines their
-symptom
-
-Calls the symptom constuctor, then outputs the instatiated symptom object
-'''
-def get_symptom():
+class Doctor:
+    def main():
+        greet_user()
+        get_basic_info()
+        get_symptom()
 
 
-def get_severity(symptom):
+    def greet_user():
+        print("Hello my name is Dr.310")
+        print("I will try to help diagnse your symptoms")
 
-def diagnose():
+    def get_basic_info():
+        name = input("What is your name?")
+        age = input("How old are you?")
+        gender = input("What is your gender?")
+        medicalHistory = input("Do you have any prior medical conditions?")
+
+    def get_symptom():
+        symptom = input("What is a symptom you are experiencing?")
+
+    def get_severity(symptom):
+        duration = input("How long have you had this symptom?")
+        pain = input("On a scale from 1 to 5, how much dicomfort does this cause you?")
+        #return duration  * pain
+        
+    def diagnose():
+
+
+
