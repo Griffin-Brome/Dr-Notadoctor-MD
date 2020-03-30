3fb1ec4da37ffb13efda35f9f35b42c677ae0e8b
wubbadukky <adcollins.d@gmail.com>
2020-03-30 14:38:27 -0600

    This should include the new dentist topics, and some new responses from the bot

--- Doctor.py
+++ Doctor.py
@@ -1,6 +1,7 @@
 from patient import patient
 from Symptom import Symptom
 import time
+import random
 
 class Doctor:
     def __init__(self):
@@ -18,6 +19,23 @@ class Doctor:
             "fatigue" : 5,
             "none" : 0
         }
+        self.genderList = {
+            "male",
+            "female",
+            "other"
+        }
+        self.physicians = {
+            "doctor",
+            "dentist"
+        }
+        self.SymptomList2 = {
+            "sore throat" : 2,
+            "sore tooth" : 3,
+            "bleeding gums" : 2,
+            "bad breath" : 1,
+            "plaque" : 1,
+            "none" : 0
+        }
         pass
 
     def greet_user(self):
@@ -26,19 +44,52 @@ class Doctor:
 
     def get_basic_info(self):
         name = input("What is your name? ")
-        age = input("Hello " + name + "! How old are you? ")
-        gender = input("What is your gender? ")
+        while True:
+            try:
+                age = int(input("Hello " + name + "! How old are you? "))
+            except ValueError:
+                print("Invalid input for age. Please try again.")
+                continue
+            else:
+                break
+
+        while True:
+            gender = str(input("What is your gender? Male, Female or other. "))
+            if gender not in self.genderList:
+                print("Invalid gender. Please try again.")
+            if gender in self.genderList:
+                break
         return patient(name, age, gender)
 
     def new_symptom(self):
+        mistakes = [ "Sorry I do not know that symptom","This is a little embarassing. But what is that?","Are you sure that's a real thing try something else"]
+
         while True:
-            symptom = str(input("What is a symptom you are experiencing? (say none for no more) ")).lower()
-            if symptom == "none":
-                return False
-            if symptom not in self.SymptomList:
-                print("Sorry, I don't recognise " + symptom)
-            if symptom in self.SymptomList:
+            physician = str(input("Are you looking for a general doctor. Or, a dentist?"))
+            if physician not in self.physicians:
+                print("Invalid option. Please try (doctor) or (dentist).")
+            if physician in self.physicians:
                 break
+
+        while True:
+            if physician == 'doctor':
+                symptom = str(input("What is a symptom you are experiencing? (say none for no more) ")).lower()
+                if symptom == "none":
+                    return False
+                if symptom not in self.SymptomList:
+                    print(random.choice(mistakes))
+                if symptom in self.SymptomList:
+                    break
+
+            if physician == 'dentist':
+                symptom = str(input("What is a symptom you are experiencing? (say none for no more) ")).lower()
+                if symptom == "none":
+                    return False
+                if symptom not in self.SymptomList2:
+                    print(random.choice(mistakes))
+                if symptom in self.SymptomList2:
+                    break
+
         duration = int(input("How many days have you had this symptom? "))
         pain = int(input("On a scale from 1 to 10, how much discomfort does this cause you? "))
         tempsymptom = Symptom(symptom)
@@ -57,19 +108,26 @@ class Doctor:
             time.sleep(.5)
             print("...")
 
-        print("Well " + patient.getName() + "...")
+        question = ["This may take a while. Any plans for the weekend?","While my diagnosis is being calculated. How's your week been?"]
+        answer = input(random.choice(question)) 
+
+        print("Could be worse I guess \n...\n Well " + patient.getName() + "...")
         if healthRating > 75:
-            print("I suggest you go to a real doctor")
+                print("I suggest you go to a real medical professional")
         elif healthRating > 50:
             print("I think you should get some rest and talk to me again tomorrow")
         else:
             print("You seem pretty healthy to me!")
+        
+        #return healthRating
 
+    # def get_severity():
+    #     duration = int(input("How many days have you had this symptom?"))
+    #     pain = int(input("On a scale from 1 to 5, how much dicomfort does this cause you?"))
+    #     return duration  * pain
 
+    # symptom = get_symptom()
+    # severity = get_severity()
 
-
-
-
-
-
-
+    # def diagnose(symp, severity):
+    #     return symp * severity
