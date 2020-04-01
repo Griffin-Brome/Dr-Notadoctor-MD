7be8f9566ab15d75e6d1b6ceadb4ce466863b27c
Griffin Brome <griffinbrome@gmail.com>
2020-04-01 14:58:28 -0700

    add Pat's changes to master (1/2)

--- python/Chatbot.py
+++ python/Chatbot.py
@@ -3,16 +3,19 @@ from Doctor import Doctor
 
 Doctor = Doctor()
 
-#greets patient and creates patient object
-Doctor.greet_user()
-patient = Doctor.get_basic_info()
 
+class Chatbot:
+    def startChat(self):
+        # greets patient and creates patient object
+        Doctor.greet_user()
+        patient = Doctor.get_basic_info()
 
-#Loop to get symptoms, right now breaks on "none"
-while True:
-    symptom = Doctor.new_symptom()
-    if(symptom == False):
-        break
-    patient.addSymptom(symptom)
-    print("Okay, ill remember that...")
-Doctor.diagnose(patient)
+        # Loop to get symptoms, right now breaks on "none"
+        while True:
+            symptom = Doctor.new_symptom()
+            if (symptom == False):
+                break
+            patient.addSymptom(symptom)
+            print("Okay, I will remember that...")
+
+        Doctor.diagnose(patient)
