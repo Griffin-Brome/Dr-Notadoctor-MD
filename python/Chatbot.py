64e5a8ea2aa1232e489fffee1b7ccc2f541b9b8f
Griffin Brome <griffinbrome@gmail.com>
2020-04-01 14:29:15 -0700

    Update Chatbot.py

--- python/Chatbot.py
+++ python/Chatbot.py
@@ -15,9 +15,4 @@ while True:
         break
     patient.addSymptom(symptom)
     print("Okay, ill remember that...")
-
-<<<<<<< HEAD:Chatbot.py
 Doctor.diagnose(patient)
-=======
-        Doctor.diagnose(patient)
->>>>>>> 2317ad6ac54479044454aa972c70fe5b7d256ffd:python/Chatbot.py
